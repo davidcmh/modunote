c1a91a6435910842e990f551e9c9618331fff0e8
davidchia <david.mhchia@gmail.com>
2016-10-16 22:30:52 +0100

    Integrate swagger framework into app - added api folder + updated server.js and config

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var express = require('express');
+var SwaggerExpress = require('swagger-express-mw');
 
 // Create app - no need to pass argument
 var app = express();
 
-// Two steps left - tell it the folder to serve + start server
+var config = {
+  appRoot: __dirname // required config
+};
 
-// use() allow you to add functionality to your app
-app.use(express.static('public'));
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
 
-// start server - listen() takes two arguments - port, callback function
-app.listen(3000, function() {
-  console.log('Express server is running on port 3000');
+  // install middleware
+  swaggerExpress.register(app);
+
+  app.use(express.static('public'));
+
+  var port = process.env.PORT || 10011;
+  app.listen(port, function() {
+    console.log('Express server is running on port ' + port);
+  });
+
+  if (swaggerExpress.runner.swagger.paths['/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  }
 });
