60b1359c620fd17ea833795d86bf3d0d600bba5a
davidchia <david.mhchia@gmail.com>
2016-12-18 18:45:31 +0000

    Modified endpoint to adapt to new data format - added contexts and topics filters, and they both accept multiple inputs

--- api/controllers/notes.js
+++ api/controllers/notes.js
@@ -6,21 +6,27 @@ var Q = require('q');
 exports.getNotes = function (req, res, next) {
     var filters = req.swagger.params.filters.value;
 
-    var mainSql = "SELECT notes.*, STRING_AGG(tag_library.name, ',') tags " +
+    var mainSql = "SELECT notes.*, contexts.name context, topics.name topic, STRING_AGG(tag_library.name, ',') tags " +
             "FROM notes " +
+            "LEFT JOIN contexts on contexts.id = notes.context_id " +
+            "LEFT JOIN topics on topics.id = notes.topic_id " +
             "LEFT JOIN tags ON notes.id = tags.note_id " +
             "LEFT JOIN tag_library ON tags.tag_id = tag_library.id ";
 
     var filterSql = "";
-    if (filters && filters.deckId) {
-        filterSql += "WHERE notes.deck_id = " + filters.deckId + " "
+    if (filters && filters.contexts) {
+        filterSql += "WHERE contexts.name IN ('" + filters.contexts.join("', '") + "') "
+    }
+    if (filters && filters.topics) {
+        var filterKeyword = filterSql.length ? 'AND ' : 'WHERE ';
+        filterSql += filterKeyword + "topics.name IN ('" + filters.topics.join("', '") + "') "
     }
     if (filters && filters.tags && filters.tags.length) {
         var filterKeyword = filterSql.length ? 'AND ' : 'WHERE ';
-        filterSql += filterKeyword + "tag_library.name in ('" + filters.tags.join("', '") + "') "
+        filterSql += filterKeyword + "tag_library.name IN ('" + filters.tags.join("', '") + "') "
     }
 
-    var groupbySql = "GROUP BY notes.id";
+    var groupbySql = "GROUP BY notes.id, contexts.name, topics.name";
 
     var query = mainSql + filterSql + groupbySql;
 
